74132c980069d5e07b21eb1264715030500eb8ba
dhoniaridho <dhoniaridho@gmail.com>
2024-04-23 09:27:43 +0700

    fix: versioning swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   const config = new DocumentBuilder()
     .setTitle('App example')
     .setDescription('The app API description')
-    .setVersion('1.0')
     .addTag('app')
     .addSecurity('JWT', { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     allowedHeaders: '*',
     origin: '*',
     credentials: true,
   });
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
+
   Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
